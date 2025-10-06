2b8faa68c06305a648a9e241fabcefc029a674f4
nguyenhongphuc290304 <nguyenhongphuc290304@gmail.com>
2025-10-06 22:17:14 +0700

    ok

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
-
 import Contact from '../pages/contact/contact'
-import Home from '../pages/home/home'
+import Home from '../pages/Home/home'
 import ProjectDetail from '../pages/project-detail/home'
-import Projects from '../pages/project/project'
+import Project from '../pages/project/project'
 import Service from '../pages/service/service'
 const Approutes = () => {
   return (
    <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<Home />} />
       <Route path="/service" element={<Service />} />
-       <Route path="/project" element={<Projects />} />
+       <Route path="/project" element={<Project />} />
          <Route path="/project/:id" element={<ProjectDetail />} />
       <Route path="/contact" element={<Contact />} />
 
